c7025c452c6de3d4ba08637cf5150cf86f010065
Monica  Karasek <mlkarasek@gmail.com>
2018-08-12 00:07:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
+      get '/' do
           erb :index
       end
 
-  post '/' do
-    erb :display_puppy
-end
+      get '/' do 
+        erb: create_puppy
+      end 
+
+      post '/' do
+          erb :display_puppy
+      end
 end 
